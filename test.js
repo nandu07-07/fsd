573b796c7dabd83e540bb05c9c74798e7ef846ad
nandu07-07 <bisainandini402@gmail.com>
2025-07-26 10:17:41 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const runTest = require('./test');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/test' && req.method === 'GET') {
+    const testResult = runTest();
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(testResult);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
