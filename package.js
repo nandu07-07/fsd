089e6c9d863fa67868e7a48c92cc352232ac1a2f
nandu07-07 <bisainandini402@gmail.com>
2025-07-26 10:15:33 +0530

    Add files via upload

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "my-node-app",
+  "version": "1.0.0",
+  "description": "A simple Node.js application",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": ["node", "app", "example"],
+  "author": "Your Name",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.2"
+  }
+}
